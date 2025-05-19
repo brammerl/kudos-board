101c1964466c5502766508599256640baf62763c
Lucia Brammer <brammerl96@gmail.com>
2025-05-19 15:39:01 -0700

    chore: Add kudos route to server

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const boards = require("./routes/board");
+const kudos = require("./routes/kudos");
 
 const PORT = 3000;
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/boards", boards);
+app.use("/kudos", kudos);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
