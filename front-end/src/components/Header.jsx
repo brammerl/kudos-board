416c62b7fd288bdbc58df28064decc79c6990aff
Lucia Brammer <brammerl96@gmail.com>
2025-05-25 18:53:37 -0700

    chore: Make header nav back to homepage

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import { AppBar, Typography } from "@mui/material";
+import { NavLink } from "react-router";
 
 const Header = () => {
   return (
     <AppBar position="static" sx={{ height: "100%" }}>
-      <Typography variant="h2">Kudos Board</Typography>
+      <NavLink to={"/"}>
+        <Typography variant="h2">Kudos Board</Typography>
+      </NavLink>
     </AppBar>
   );
 };
